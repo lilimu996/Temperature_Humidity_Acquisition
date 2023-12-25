5aa5f5ab9cc95406220d02ec6b1488c2c33e251d
lilimu996 <junhaolics@foxmail.com>
2023-12-25 17:05:54 +0800

    移植lvgl8.2

---
+++ Project/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Temperature_Humidity_Acquisition' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SWM341.h"
+
+/* Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+
+
+#endif /* RTE_COMPONENTS_H */
